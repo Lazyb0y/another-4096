4f11803a12ab8e8da47474e7a4fc97028fbab6c6
Lazyb0y <thisisfoysal@gmail.com>
2018-08-12 16:01:45 +0600

    Resizing the game to cover the wider area possible and binding it with window resize event

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,22 @@
 var Another4096 = Another4096 || {};
 
+/* Resizing the game to cover the wider area possible */
+function resizeGame() {
+    var canvas = document.querySelector("canvas");
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var windowRatio = windowWidth / windowHeight;
+    var gameRatio = Another4096.game.config.width / Another4096.game.config.height;
+    if (windowRatio < gameRatio) {
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else {
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
+}
+
 window.onload = function () {
     Another4096.GameConfig = {
         width: 480,
@@ -9,4 +26,6 @@ window.onload = function () {
 
     /* Initializing the Phaser 3 framework */
     Another4096.game = new Phaser.Game(Another4096.GameConfig);
+    resizeGame();
+    window.addEventListener("resize", resizeGame);
 };
